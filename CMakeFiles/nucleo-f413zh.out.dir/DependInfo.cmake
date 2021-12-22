fac4dde0d667fb0b92fe49d412e2b9ae346be79e
WIN-OQJMAHBKE9E\Administrator <13611858691@163.com>
2021-12-22 18:12:44 +0800

    openocd

---
+++ CMakeFiles/nucleo-f413zh.out.dir/DependInfo.cmake
@@ -0,0 +1,61 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "D:/DESK/github/stm32_cmake_example/startup_stm32f413xx.s" "D:/DESK/github/stm32_cmake_example/CMakeFiles/nucleo-f413zh.out.dir/startup_stm32f413xx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32F413xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "Core/Inc"
+  "Drivers/STM32F4xx_HAL_Driver/Inc"
+  "Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/DESK/github/stm32_cmake_example/Core/Src/main.c" "CMakeFiles/nucleo-f413zh.out.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/nucleo-f413zh.out.dir/Core/Src/main.c.obj.d"
+  "D:/DESK/github/stm32_cmake_example/Core/Src/stm32f4xx_hal_msp.c" "CMakeFiles/nucleo-f413zh.out.dir/Core/Src/stm32f4xx_hal_msp.c.obj" "gcc" "CMakeFiles/nucleo-f413zh.out.dir/Core/Src/stm32f4xx_hal_msp.c.obj.d"
+  "D:/DESK/github/stm32_cmake_example/Core/Src/stm32f4xx_it.c" "CMakeFiles/nucleo-f413zh.out.dir/Core/Src/stm32f4xx_it.c.obj" "gcc" "CMakeFiles/nucleo-f413zh.out.dir/Core/Src/stm32f4xx_it.c.obj.d"
+  "D:/DESK/github/stm32_cmake_example/Core/Src/system_stm32f4xx.c" "CMakeFiles/nucleo-f413zh.out.dir/Core/Src/system_stm32f4xx.c.obj" "gcc" "CMakeFiles/nucleo-f413zh.out.dir/Core/Src/system_stm32f4xx.c.obj.d"
+  "D:/DESK/github/stm32_cmake_example/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj" "gcc" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj.d"
+  "D:/DESK/github/stm32_cmake_example/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj" "gcc" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj.d"
+  "D:/DESK/github/stm32_cmake_example/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj" "gcc" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj.d"
+  "D:/DESK/github/stm32_cmake_example/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj.d"
+  "D:/DESK/github/stm32_cmake_example/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj" "gcc" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj.d"
+  "D:/DESK/github/stm32_cmake_example/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj" "gcc" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj.d"
+  "D:/DESK/github/stm32_cmake_example/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj.d"
+  "D:/DESK/github/stm32_cmake_example/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj" "gcc" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj.d"
+  "D:/DESK/github/stm32_cmake_example/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj" "gcc" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj.d"
+  "D:/DESK/github/stm32_cmake_example/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pcd.c" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pcd.c.obj" "gcc" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pcd.c.obj.d"
+  "D:/DESK/github/stm32_cmake_example/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pcd_ex.c" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pcd_ex.c.obj" "gcc" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pcd_ex.c.obj.d"
+  "D:/DESK/github/stm32_cmake_example/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj" "gcc" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj.d"
+  "D:/DESK/github/stm32_cmake_example/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj.d"
+  "D:/DESK/github/stm32_cmake_example/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj" "gcc" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj.d"
+  "D:/DESK/github/stm32_cmake_example/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj.d"
+  "D:/DESK/github/stm32_cmake_example/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj" "gcc" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj.d"
+  "D:/DESK/github/stm32_cmake_example/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj.d"
+  "D:/DESK/github/stm32_cmake_example/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c.obj" "gcc" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c.obj.d"
+  "D:/DESK/github/stm32_cmake_example/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_usb.c" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_usb.c.obj" "gcc" "CMakeFiles/nucleo-f413zh.out.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_usb.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/nucleo-f413zh.out.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for nucleo-f413zh.out.
+# This may be replaced when dependencies are built.
